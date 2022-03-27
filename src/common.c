18859020b9c4f574a046616ffdeab858c5ea5ab1
dr-fisk <angelgarcia112198@gmail.com>
2022-03-26 19:12:20 -0700

    Added quick documentation of all functions

--- src/common.c
+++ src/common.c
@@ -1,17 +1,33 @@
 #include "common.h"
 
+/* Function:    GLClearError
+   Description: Clears current opengl error
+   Parameters:  None
+   Returns:     None 
+ */
 void GLClearError() {
   while(glGetError() != GL_NO_ERROR);
 }
 
+/* Function:    GLLogCall
+   Description: Stores opengl error to output file when opengl complains about misuse
+                of functions
+                Use of function is to wrap around opengl call ex.
+                GLLogCall(glDrawArrays())
+   Parameters:  char* - Function name where error occurred
+                char* - File name where error occurred
+                int   - Line number where error occurred
+   Returns:     bool  - Error detected
+ */
 bool GLLogCall(const char* function, const char *file, int line) {
   std::ofstream fp("errorLog.txt");
   
-  while (GLenum error = glGetError()) {
+  if (GLenum error = glGetError()) {
     fp << "[OpenGL Error] (" << error << ") In " << function << " located in " << file << " on line number " << line << std::endl;
     fp.close();
     return 0;
   }
+  
   fp.close();
   return 1;
 }
--- src/indexBuffer.c
+++ src/indexBuffer.c
@@ -1,19 +1,41 @@
 #include "indexBuffer.h"
 
+/* Function:    IndexBuffer
+   Description: Creates IndexBuffer as an Element Array Buffer
+   Parameters:  uint32_t* - Indeces describing VBO
+                uint32_t  - Size of array
+   Returns:     None 
+ */
 IndexBuffer::IndexBuffer(const uint32_t *data, uint32_t count) : count(count) {
   GLCall(glGenBuffers(1, &indexID));
   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexID));
   GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), data, GL_STATIC_DRAW));
 }
 
+/* Function:    ~IndexBuffer
+   Description: Destroys IndexBuffer
+   Parameters:  None
+   Returns:     None 
+ */
 IndexBuffer::~IndexBuffer() {
   glDeleteBuffers(1, &indexID);
 }
 
+/* Function:    bind
+   Description: Prepares IBO to be rendered
+                Always call before drawing
+   Parameters:  None
+   Returns:     None 
+ */
 void IndexBuffer::bind() const {
   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexID));
 }
 
+/* Function:    unbind
+   Description: Cleans up IBO after being drawn
+   Parameters:  None
+   Returns:     None 
+ */
 void IndexBuffer::unbind() const {
   GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 }
